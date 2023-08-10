b5252efc61e1ec07cc15a3b9397996c3383422f4
gyp <gyp@gypdeMacBook-Pro.local>
2023-08-10 11:13:57 +0800

    add DCGAN network

--- Champter8_生成式深度学习/8.6_DCGAN.py
+++ Champter8_生成式深度学习/8.6_DCGAN.py
@@ -10,3 +10,59 @@
 
 # import some packages
 # --------------------
+import torch.nn as nn
+
+# DCGAN在GAN的基础上优化了网络结构，加入了卷积层(Conv)，转置卷积(ConvTranspose)、批量正则(Batch_norm)等层，使得网络更容易训练。
+
+class Discriminator(nn.Module):
+    """
+    使用卷积层、批规范层的判别器
+    """
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),  # 输入通道数，输出通道数，kernel_size，stride, padding
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*2, ndf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*4, ndf*8, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ndf*8),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(ndf*8, 1, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input):
+        return self.model(input)
+
+
+class Generator(nn.Module):
+    """使用转置卷积、批规范层的生成器"""
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            # 输入Z， nz表示Z的大小, ngf为生成器特征图大小, nc为通道数
+            nn.ConvTranspose2d(nz, ngf*8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf*8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf*8, ngf*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*4),
+            nn.ReLU(True),
+            # state size (ngf*4) * 8 * 8
+            nn.ConvTranspose2d(ngf*4, ngf*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf*2),
+            nn.ReLU(True),
+            # state size (ngf*2) * 16 * 16
+            nn.ConvTranspose2d(ngf*2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+        )
+
+    def forward(self, input):
+        return self.model(input)
